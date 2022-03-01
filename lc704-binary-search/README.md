110998c06c14fe26a8f907e7e309c96e82009d86
TangHaomiao <tanghaomiao@campuses.cn>
2022-03-01 21:00:49 +0800

    + leetcode 704 binary-search

---
+++ lc704-binary-search/README.md
@@ -0,0 +1,4 @@
+Use a head and tail pointer to indicate current search area: 
+
+- The search stops when the length of the search area is 0. The expression of stop condition varies according to the definition of head and tail pointers.
+- The accessed element should be excluded from the next search area.
---
+++ lc704-binary-search/binary-search.py
@@ -0,0 +1,18 @@
+from typing import List
+
+
+def search(nums: List[int], target: int) -> int:
+    head = 0
+    tail = len(nums)
+    while head != tail:
+        p = int((head + tail) / 2)
+        if nums[p] == target:
+            return p
+        elif nums[p] > target:
+            tail = p
+        elif nums[p] < target:
+            head = p + 1
+    return -1
+
+
+print(search([-1, 0, 3, 5, 9, 12], 9))
